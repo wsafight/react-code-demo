d26dcafa1b48d605382229a542e67b23c2fc685d
wsafight <984292420@qq.com>
2022-12-06 02:44:21 +0800

    #feat: change

--- src/immutability-helper.js
+++ src/immutability-helper.js
@@ -40,14 +40,14 @@ const getSchools = () => {
   for(let i = 0; i< 10000; i++) {
     schools.push({
       name: `学校${i}`,
-      xx: '13',
-      pp: '13221'
+      start: '13',
+      end: '13221'
     })
   }
   return schools
 }
 
-function App() {
+function App2() {
 
   const [user, setUser] = useState({
     name: 'wsa',
@@ -58,18 +58,7 @@ function App() {
       { name: '测试小学' },
       { name: '测试高中' }
     ].concat(...getSchools()),
-    schools2: [
-      { name: '测试小学' },
-      { name: '测试高中' }
-    ].concat(...getSchools()),
-    schools4: [
-      { name: '测试小学' },
-      { name: '测试高中' }
-    ].concat(...getSchools()),
-    schools5: [
-      { name: '测试小学' },
-      { name: '测试高中' }
-    ].concat(...getSchools()),
+
   });
 
 
@@ -167,4 +156,96 @@ function App() {
   );
 }
 
+
+function App() {
+  const [user, setUser] = useState({
+    name: 'wsa',
+    company: {
+      name: '测试公司'
+    },
+    schools: [
+      { name: '测试小学', start: '1998-01-02', end: '2004-01-02' },
+      { name: '测试高中', start: '2005-01-02', end: '2007-01-02' },
+    ].concat(...getSchools()),
+    schools2: [
+      { name: '测试小学', start: '1998-01-02', end: '2004-01-02' },
+      { name: '测试高中', start: '2005-01-02', end: '2007-01-02' },
+    ].concat(...getSchools()),
+    schools3: [
+      { name: '测试小学', start: '1998-01-02', end: '2004-01-02' },
+      { name: '测试高中', start: '2005-01-02', end: '2007-01-02' },
+    ].concat(...getSchools()),
+    schools4: [
+      { name: '测试小学', start: '1998-01-02', end: '2004-01-02' },
+      { name: '测试高中', start: '2005-01-02', end: '2007-01-02' },
+    ].concat(...getSchools()),
+    schools5: [
+      { name: '测试小学', start: '1998-01-02', end: '2004-01-02' },
+      { name: '测试高中', start: '2005-01-02', end: '2007-01-02' },
+    ].concat(...getSchools()),
+    // schools6: [
+    //   { name: '测试小学', start: '1998-01-02', end: '2004-01-02' },
+    //   { name: '测试高中', start: '2005-01-02', end: '2007-01-02' },
+    // ].concat(...getSchools()),
+  });
+
+  /**
+   * Profiler 组件，可以查看渲染时间等信息
+   */
+  const renderCallback = (...info) => {
+    console.log(`渲染原因 ${info[1]},本次更新 committed 花费的渲染时间 ${info[2]}`);
+  }
+
+  const handleSchools = () => {
+    user.schools[0].name = '1232222'
+    setUser({...user})
+  }
+
+  const handleSchools2 = () => {
+    console.time('测试')
+    const bb = update(user, convertImmutabilityByPath('schools[0].name' ,{
+      $set: 'bbv'
+    },))
+    console.timeEnd('测试')
+    setUser(bb)
+  }
+
+  const handleSchools3 = () => {
+    user.schools[0].name = '1232222'
+    console.time('测试2')
+    const user2 = JSON.parse(JSON.stringify(user))
+    console.timeEnd('测试2')
+    setUser(user2)
+  }
+
+  // 使用 useMemo 优化性能，如果是新的组件使用 memo 或者 pureComponent
+  // 如果 user.schools 不变,则不会重新渲染
+  const renderSchools = useMemo(() => {
+    return (
+      <div>
+        {user.schools.map(item => {
+          return <div key={item.name}>
+            {item.name}
+            {item.start}
+            {item.end}
+          </div>
+        })}
+      </div>
+    );
+  }, [user.schools]);
+
+  return (
+    <div className="App">
+      <Profiler id="render" onRender={renderCallback}>
+        <header className="App-header">
+          {user.name}
+          <button onClick={handleSchools}>修改学校</button>
+          <button onClick={handleSchools2}>修改学校</button>
+          <button onClick={handleSchools3}>修改学校</button>
+          <div>{renderSchools}</div>
+        </header>
+      </Profiler>
+    </div>
+  );
+}
 export default App;
