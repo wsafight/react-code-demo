8b66f3620dae80ebe405bfc5c425324faced19a4
wsafight <984292420@qq.com>
2023-01-10 03:28:42 +0800

    #FEAT: add cliboard

--- src/copy.js
+++ src/copy.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { copyImageToClipboard, copyTextToClipboard } from 'clipboard-helper'
+import React, { useRef } from 'react';
+import { copyTextToClipboard, copyFileToClipboard ,readTextFromClipboard } from 'clipboard-helper'
 
 function App() {
   const handleCopy = () => {
@@ -10,22 +10,31 @@ function App() {
     })
   }
 
+  const bb = useRef(null);
+
   const handleCopy2 = () => {
-    copyImageToClipboard('https://img.alicdn.com/bao/uploaded/i4/1959203576/O1CN01Fao8rf1cHr5JWAzuy_!!1959203576.png').then(res => {
+    copyFileToClipboard('https://img.alicdn.com/bao/uploaded/i4/1959203576/O1CN01Fao8rf1cHr5JWAzuy_!!1959203576.png').then(res => {
       console.log('test-res', res);
     }).catch(error => {
       console.log('test-err', error)
     })
   }
 
+
   const handleCopy3 = () => {
     const img = document.getElementById('image');
     console.log('tt', img);
     console.log(img instanceof HTMLImageElement);
-    copyImageToClipboard(img).then(res => {
+    copyFileToClipboard(bb.current).then(res => {
       console.log('test-res', res);
     }).catch(error => {
       console.log('test-err', error)
+    })
+  }
+
+  const handleCopy4 =() => {
+    readTextFromClipboard().then(res => {
+      console.log('wsa-res', res);
     })
   }
 
@@ -34,9 +43,10 @@ function App() {
       <button onClick={handleCopy} >拷贝</button>
 
       <button onClick={handleCopy2} >拷贝图片</button>
-      <img id='image' width={200} src='https://img.alicdn.com/bao/uploaded/i4/1959203576/O1CN01Fao8rf1cHr5JWAzuy_!!1959203576.png' />
+      <img id='image' ref={bb} width={200} src='https://img.alicdn.com/bao/uploaded/i4/1959203576/O1CN01Fao8rf1cHr5JWAzuy_!!1959203576.png' />
 
       <button onClick={handleCopy3} >拷贝图片3</button>
+      <button onClick={handleCopy4} >获取文本</button>
     </div>
   );
 }
